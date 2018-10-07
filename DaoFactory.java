28e62e355f84437f5cabcd465070d6004fe0b2c0
tlaabs <tlaabs@naver.com>
2018-10-07 21:15:14 +0900

    DataSource & setting applicationContext by using xml

--- DaoFactory.java
+++ DaoFactory.java
@@ -1,5 +1,8 @@
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 //Application Context가 사용할 설정정보라는 표시
 @Configuration
@@ -7,12 +10,25 @@ public class DaoFactory {
 	
 	@Bean //오브젝트 생성을 담당하는 IoC용 메소드라는 표시
 	public UserDao userDao() {
-		UserDao userDao = new UserDao(connectionMaker());
+		UserDao userDao = new UserDao();
+		userDao.setDataSource(dataSource());
 		return userDao;
 	}
 	
+//	@Bean
+//	public ConnectionMaker connectionMaker() {
+//		return new DConnectionMaker();
+//	}
+	
 	@Bean
-	public ConnectionMaker connectionMaker() {
-		return new DConnectionMaker();
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/springbook?serverTimezone=UTC&useSSL=false");
+		dataSource.setUsername("spring");
+		dataSource.setPassword("book");
+		
+		return dataSource;
+		
 	}
 }
--- UserDao.java
+++ UserDao.java
@@ -4,6 +4,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 /*
  * 작성자 : 심세용
  * 업데이트 : 2018-10-04
@@ -11,46 +13,63 @@ import java.sql.SQLException;
  */
 
 public class UserDao {
-	private ConnectionMaker connectionMaker; //초기에 설정하면 바뀔 일이 없지만,
-	//왜냐하면 @Bean을 붙여만들었기 떄문에 이 자체도 싱글톤임.
 	
-	public UserDao(ConnectionMaker connectionMaker) {
-		this.connectionMaker = connectionMaker;
+	//DB 커넥션등의 기능을 쉽게할 수 있도록 도와주는 클래스.
+	private DataSource dataSource;
+	
+//	private ConnectionMaker connectionMaker; // 초기에 설정하면 바뀔 일이 없지만,
+	// 왜냐하면 @Bean을 붙여만들었기 떄문에 이 자체도 싱글톤임.
+
+//	public UserDao(ConnectionMaker connectionMaker) {
+//		this.connectionMaker = connectionMaker;
+// 의존관계 검색
+//		AnnotationConfigApplicationContext context =
+//				new AnnotationConfigApplicationContext(DaoFactory.class);
+//		this.connectionMaker = context.getBean("connectionMaker", ConnectionMaker.class);
+//	}
+	
+//	public void setConnectionMaker(ConnectionMaker connectionMaker) {
+//		this.connectionMaker = connectionMaker;
+//	}
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
+	
+	
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = connectionMaker.makeConnection();
-		
-		PreparedStatement ps = c.prepareStatement(
-				"insert into users(id, name, password) values(?,?,?)");
+		Connection c = dataSource.getConnection();
+
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getPassword());
-		
+
 		ps.executeUpdate();
-		
+
 		ps.close();
 		c.close();
 	}
-	
+
+
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = connectionMaker.makeConnection();
-		
-		PreparedStatement ps = c.prepareStatement(
-				"select * from users where id = ?");
+		Connection c = dataSource.getConnection();
+
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
-		
+
 		ResultSet rs = ps.executeQuery();
 		rs.next();
 		User user = new User();
 		user.setId(rs.getString("id"));
 		user.setName(rs.getString("name"));
 		user.setPassword(rs.getString("password"));
-		
+
 		rs.close();
 		ps.close();
 		c.close();
-		
+
 		return user;
-		
+
 	}
 }

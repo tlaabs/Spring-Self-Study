3e2bb3df804245bfb501836d0ca9089537461b0d
tlaabs <tlaabs@naver.com>
2018-10-15 19:28:03 +0900

    upgradeLevels() 리팩토링

--- springbook/user/dao/Level.java
+++ springbook/user/dao/Level.java
@@ -3,16 +3,22 @@ package springbook.user.dao;
 public enum Level {
 	//클래스와 비교했을 때 Enum이 가지는 장점이? 타입체크 가능.
 	//http://limkydev.tistory.com/50
-	BASIC(1), SILVER(2), GOLD(3);
+	GOLD(3,null), SILVER(2,GOLD), BASIC(1,SILVER);
 
 	private final int value;
-
-	Level(int value) {
+	private final Level next;
+	
+	Level(int value, Level next) {
 		this.value = value;
+		this.next = next;
 	}
 
 	public int intValue() {
 		return value;
+	}
+
+	public Level nextLevel() {
+		return this.next;
 	}
 
 	public static Level valueOf(int value) {

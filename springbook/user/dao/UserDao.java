6b07cb5165ee5c7286117dc9e362be069f07265c
tlaabs <tlaabs@naver.com>
2018-10-17 20:05:59 +0900

    UserService 트랜잭션 경계설정의 문제점

--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -1,11 +1,18 @@
 package springbook.user.dao;
+import java.sql.Connection;
 import java.util.List;
 
 public interface UserDao {
-	void add(User user);
-	User get(String id);
+	/**
+	 * Connection 파라미터가 추가되면 더 이상 데이터 액세스 기술에 독립적일 수가 없다.
+	 * JPA나 하이버네이트로 구현하게되면 Connection 대신 다른 오브젝트를 사용함.
+	 * 테스트 코드까지 변질되었다.
+	 */
+	
+	void add(Connection c, User user);
+	User get(Connection c, String id);
 	List<User> getAll();
 	void deleteAll();
 	int getCount();
-	public void update(User user1);
+	public void update(Connection c, User user1);
 }
--- springbook/user/service/UserService.java
+++ springbook/user/service/UserService.java
@@ -1,6 +1,6 @@
 package springbook.user.service;
 
-import java.util.List;
+import java.sql.Connection;
 
 import springbook.user.dao.Level;
 import springbook.user.dao.User;
@@ -26,20 +26,38 @@ public class UserService{
 
 	//Policy 적용
 	public void upgradeLevels() {
-		List<User> users = userDao.getAll();
-		for (User user : users) {
-			if(canUpgradeLevel(user)) {
-				upgradeLevel(user);
-			}
-		}
+//		List<User> users = userDao.getAll();
+//		for (User user : users) {
+//			if(canUpgradeLevel(user)) {
+//				upgradeLevel(user);
+//			}
+//		}
+		
+		/**
+		 * try/catch/finally 블록이 서비스내에 존재하고 JDBC 작업 코드의 전형적인 문제점을 그대로 가질 수 밖에 없다.
+		 * 또한 서비스내의 인스턴스 변수에 Connection을 저장해뒀다가 다른 메소드에서 사용할 수도 없다.
+		 * 왜냐하면 서비스가 스프링 빈으로 선언되서 싱글톤으로 되어있기 떄문이다
+		 * 멀티스레드 환경에서 공유하는 인스턴스 변수에 정보를 저장하다가는 서로 덮어쓰는 일이 발생할 수 있다.
+		 */
+		
+//		Connection c = ...
+//		try {
+//			upgradeLevel(c, user);
+//		}catch() {
+//			
+//		}finally{
+//			
+//		}
+//		..
 	}
 	
 	private boolean canUpgradeLevel(User user) {
 		return levelPolicy.canUpgradeLevel(user); 
 	}
 	
-	protected void upgradeLevel(User user) {
+	protected void upgradeLevel(Connection c, User user) {
 		levelPolicy.upgradeLevel(user);
+		userDao.update(c, user);
 	}
 
 	public void add(User user) {

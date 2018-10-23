db96e10bab51a218f3d9c55aae3db2f96e87fed1
tlaabs <tlaabs@naver.com>
2018-10-23 21:14:02 +0900

    /

--- springbook/user/dao/Main.java
+++ springbook/user/dao/Main.java
@@ -5,8 +5,7 @@ public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		// TODO Auto-generated method stub
-//		UserDao dao = new UserDao();
-//		
+//		UserDao dao = new UserDao();	
 //		User user = new User();
 //		user.setId("whiteship");
 //		user.setName("��⼱");

1efd185962c8daf3931c9ae41a6ec808d5dd479a
tlaabs <tlaabs@naver.com>
2018-11-12 21:35:10 +0900

    @Transactional 적용

--- springbook/user/service/UserService.java
+++ springbook/user/service/UserService.java
@@ -2,16 +2,21 @@ package springbook.user.service;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import springbook.user.dao.User;
 
+@Transactional
 public interface UserService {
 	void add(User user);
-	
 	//DAO메소드와 1:1대응되는 CRUD 메소드이지만 단순 위임 이상의 로직을 가질 수 있다.(다른 트랜잭션에 참여할 가능성이 높다)
-	User get(String id);
-	List<User> getAll();
 	void deleteAll();
 	void update(User user);
-	
 	void upgradeLevels();
+	
+	@Transactional(readOnly=true)
+	User get(String id);
+	
+	@Transactional(readOnly=true)
+	List<User> getAll();
 }
